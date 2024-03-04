5f1bcd56d657b6a5fcc82eef273adfb1ac2eeba4
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2024-03-04 14:18:10 +0300

    Angular ile frontend geliştirilmeye başlandı.

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product';
+import { HttpClient } from '@angular/common/http';
+import { ProductResponseModel } from '../../models/productResponseModel';
+import { ProductService } from '../../services/product.service';
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent implements OnInit {
+  products:Product[] = []
+  dataLoaded = false;
+  
+  constructor(private productService:ProductService){};
+
+  ngOnInit(): void {
+    this.getProducts();
+    
+  }
+  getProducts(){
+    this.productService.getProducts().subscribe(response=>{
+      this.products= response.data
+      this.dataLoaded=true;
+    });
+  }
+
+}
