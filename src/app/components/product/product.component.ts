5290d6ded0c1195d1f932167d6992a46cb5d82be
Tahsin Emre Öztürk <tahsinemrre@gmail.com>
2024-03-19 05:29:55 +0300

    İlk pipe yapısı oluşturuldu.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -11,6 +11,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductComponent implements OnInit {
   products:Product[] = []
   dataLoaded = false;
+  filterText="";
   
   constructor(private productService:ProductService,
     private activatedRoute:ActivatedRoute){};
